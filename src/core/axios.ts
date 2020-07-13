64991697be8089e673e07bf769e2d0229d1d8e0e
HarryXiong24 <haoweixiong@gmail.com>
2020-07-13 21:35:55 +0800

    feat: 0.1.2

--- src/core/axios.ts
+++ src/core/axios.ts
@@ -1,14 +1,33 @@
 import dispatchRequest from './dispatchRequest'
 import { AxiosRequestConfig } from '../types/AxiosRequest'
-import { AxiosPromise } from '../types/AxiosResponse'
+import { AxiosPromise, AxiosResponse } from '../types/AxiosResponse'
 import { Method } from '../types/method'
+import { ResolvedFn, RejectedFn } from '../types/AxiosInterceptor'
+import InterceptorManager from './Interceptor'
 
+interface Interceptors {
+  request: InterceptorManager<AxiosRequestConfig>
+  response: InterceptorManager<AxiosResponse>
+}
+
+interface PromiseChain {
+  resolved: ResolvedFn | ((config: AxiosRequestConfig) => AxiosPromise)
+  rejected?: RejectedFn
+}
 /**
  * 创建一个 core 目录，用来存放发送请求核心流程的代码。
  * 我们在 core 目录下创建 Axios.ts 文件。
  * 创建一个 Axios 类，来实现接口定义的公共方法。
  */
 export default class Axios {
+  interceptors: Interceptors
+
+  constructor() {
+    this.interceptors = {
+      request: new InterceptorManager<AxiosRequestConfig>(),
+      response: new InterceptorManager<AxiosResponse>()
+    }
+  }
   // 支持axios传1-2个参数
   request(url: any, config?: any): AxiosPromise {
     if (typeof url === 'string') {
@@ -19,7 +38,29 @@ export default class Axios {
     } else {
       config = url
     }
-    return dispatchRequest(config)
+    const chain: PromiseChain[] = [
+      {
+        resolved: dispatchRequest,
+        rejected: undefined
+      }
+    ]
+
+    this.interceptors.request.forEach(interceptor => {
+      chain.unshift(interceptor)
+    })
+
+    this.interceptors.response.forEach(interceptor => {
+      chain.push(interceptor)
+    })
+
+    let promise = Promise.resolve(config)
+
+    while (chain.length) {
+      const { resolved, rejected } = chain.shift()!
+      promise = promise.then(resolved, rejected)
+    }
+
+    return promise
   }
 
   get(url: string, config?: AxiosRequestConfig): AxiosPromise {
